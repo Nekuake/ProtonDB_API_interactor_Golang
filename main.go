27c2caef7905421085fbb9ca21abaed269fbd167
Nekuake <ruizale55@gmail.com>
2022-12-24 19:57:58 +0100

    Frankenstein code at Christmas Eve

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func initializeData() {
+	response, err := http.Get("https://protondb.max-p.me/reports.json")
+}
+
+func getData(gameID string) {
+}
+
 func main() {
+
+	fmt.Println("Input Steam game ID")
+	var gameId string
+	fmt.scanln(&gameId)
 
 }
