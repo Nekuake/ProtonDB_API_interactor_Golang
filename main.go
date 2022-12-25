9a902bd83f230eee1e50e1eaa3a95865a5a42e4d
Nekuake <ruizale55@gmail.com>
2022-12-25 13:00:29 +0100

    Quick progress on Christmas

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-func initializeData() {
-	response, err := http.Get("https://protondb.max-p.me/reports.json")
+func initializeData() map[string]interface{} {
+	resp, err := http.Get("https://protondb.max-p.me/reports.json")
+	if err != nil {
+		fmt.Printf("Error while making request: %v\n", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	// Read content of request
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error while reading content: %v\n", err)
+		return nil
+	}
+
+	// JSON -> map
+	var protonMap map[string]interface{}
+	err = json.Unmarshal(body, &protonMap)
+	if err != nil {
+		fmt.Printf("Error while paring JSON: %v\n", err)
+		return nil
+	}
+	return protonMap
 }
 
-func getData(gameID string) {
+func getData(gameID string, gameDB map) {
+
 }
 
 func main() {
+	fmt.Println("Downloading protonDB Json...")
 
 	fmt.Println("Input Steam game ID")
 	var gameId string
-	fmt.scanln(&gameId)
-
+	fmt.Scanln(&gameId)
 }
